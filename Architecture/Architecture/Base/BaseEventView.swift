c10fabd4ff5dc54fa101cbe1a288eb5b6ca826b2
Raymund <raymund.catahay@majority.com>
2021-01-23 18:25:19 +0100

    added anchorage for layout sugar;

--- Architecture/Architecture/Base/BaseEventView.swift
+++ Architecture/Architecture/Base/BaseEventView.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Eventful
+import Anchorage
 
 open class BaseEventRootView<BaseEventViewEvent: ViewEvent, BaseEventPresentableEvent: PresentableEvent>: EventRootView<BaseEventViewEvent, BaseEventPresentableEvent>, BaseEventViewControllerEventListener, BaseEventCorePresentableEventListener {
     
