ec0fcdb9a6866567df50e52c28d04bbcf6e33fcc
Raymund <raymund.catahay@majority.com>
2021-01-23 18:25:30 +0100

    added initial table view

--- QueryTableScreen/QueryTableScreen/Sources/QueryTableView.swift
+++ QueryTableScreen/QueryTableScreen/Sources/QueryTableView.swift
@@ -8,13 +8,29 @@
 import UIKit
 import Eventful
 import Architecture
+import Anchorage
 
 public enum QueryTableViewEvent: ViewEvent { }
 
 public  class QueryTableView: BaseEventRootView<QueryTableViewEvent, QueryTablePresentableEvent> {
     
+    //MARK: Subviews
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
     public override func setup() {
+        /// Self setup
+        backgroundColor = .white
         
+        /// Children
+        addSubview(tableView)
+        tableView.topAnchor == safeAreaLayoutGuide.topAnchor
+        tableView.bottomAnchor == safeAreaLayoutGuide.bottomAnchor
+        tableView.leadingAnchor == leadingAnchor
+        tableView.trailingAnchor == trailingAnchor
     }
     
     public override func presenter(didSend event: QueryTablePresentableEvent) {
