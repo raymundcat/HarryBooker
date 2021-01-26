94b1d1bebeecb4e3eada83b91b8edbc2486a12f6
Raymund <raymund.catahay@majority.com>
2021-01-26 19:14:19 +0100

    added test info

--- HarryBookerTests/QueryTableTests/QueryTableErrorHandlingTest.swift
+++ HarryBookerTests/QueryTableTests/QueryTableErrorHandlingTest.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+import XCTest
+import CasePaths
+import PromiseKit
+import OHHTTPStubs
+import OHHTTPStubsSwift
+@testable import HarryBooker
+
+class QueryTableErrorHandlingTest: StubbedTests {
+    
+    override class func setUp() {
+        stub { (request) -> Bool in
+            return request.url?.host == "api.storytel.net" &&
+                request.url?.valueOf("query") == "Harry"
+        } response: { (request) -> HTTPStubsResponse in
+            let error = NSError(
+                    domain: "test",
+                    code: 500,
+                    userInfo: [:])
+            return HTTPStubsResponse(error: error)
+        }
+    }
+    
+    func testFirstOnLoadValue() {
+        
+        let presenter = QueryTablePresenter(query: "Harry")
+        
+        let bucket = PresentableBucket<BaseEventCorePresentableEvent>()
+        presenter.presentables.addDelegate(bucket)
+        
+        /// Wake up the presenter
+        presenter.viewController(didSend: .viewDidLoad)
+        
+        /// Set the expectations
+        let expectation = self.expectation(description: "Expecting for books")
+        
+        firstly {
+            after(seconds: 2.0)
+        }.done { _ in
+            
+            /// Catch an elert event
+            let alertEvent = bucket.events.compactMap(/BaseEventCorePresentableEvent.showAlert).last
+            
+            /// Assert that an alert has been shown
+            XCTAssertNotNil(alertEvent)
+            
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5)
+    }
+}
+
